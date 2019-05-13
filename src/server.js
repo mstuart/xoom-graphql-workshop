1b317d35e739f0c6e74e39f6688cf100fe09a1c3
marstuart <marstuart@paypal.com>
2019-05-12 23:31:04 -0700

    04-deduping-solution

--- src/server.js
+++ src/server.js
@@ -104,7 +104,7 @@ const typeDefs = `
 const resolvers = {
   Query: {
     albums: async (rootObj, { albumId, userId }) => {
-      const albums = await get('/albums');
+      const albums = await dataLoader.load('/albums');
 
       if (albumId) {
         return albums.filter(album => album.id === Number(albumId));
@@ -118,7 +118,7 @@ const resolvers = {
     },
 
     album: async (rootObj, { albumId }) => {
-      const albums = await get('/albums');
+      const albums = await dataLoader.load('/albums');
 
       return albums.find(album => album.id === Number(albumId));
     }
@@ -127,26 +127,7 @@ const resolvers = {
   Album: {
     albumId: ({ id }) => id,
 
-    // EXERCISE #4 -- Oops, we have a problem!
-    // We're causing unnecessary pressure on the https://jsonplaceholder.typicode.com API.
-    // Surprised they haven't shut us off yet :-)
-    //
-    // See the console.log line on line 9 (up above).
-    // Go back to GraphQL Playground and request all albums (no arguments) with their associated users.
-    // Now, check your console for the console.log() message.
-    // 💩!  We're making the same API call a bunch of times!
-    //
-    // Part #1 --
-    // We need to dedupe these API calls.
-    // There are a couple options out there, but let's create an Apollo data source.
-    // Apollo data sources will dedupe API calls to ensure we're always fetching efficiently.
-    // Follow the docs at https://www.apollographql.com/docs/apollo-server/features/data-sources
-    //
-    // Once you are using your new data source, use it and uncomment the console.log() line at line 9 again (up above).
-    // If it's working properly, you'll see that we no longer overfetch.
-    //
-    // Sweet!  Now we're fetching optimally.  We are so cool!  Wow!  :-D
-    user: async ({ userId }) => await get(`/users/${userId}`)
+    user: async ({ userId }) => await dataLoader.load(`/users/${userId}`)
   },
 
   User: {
