52d5df00408ac49a6e15e3cf2e6bba0f6dd90020
marstuart <marstuart@paypal.com>
2019-07-05 17:25:47 -0700

    Update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const { ApolloServer } = require('apollo-server');
 const axios = require('axios');
+const https = require('https');
 
 const client = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com'
+  baseURL: 'https://jsonplaceholder.typicode.com',
+
+  // May get SSL errors w/ depending on your network,
+  // so we will ignore any SSL errors for now.
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
 });
 
 const get = async url => client.get(url).then(({ data }) => data);
@@ -10,15 +17,19 @@ const get = async url => client.get(url).then(({ data }) => data);
 const typeDefs = `
   type Query {
     # Returns all albums
-    albums(albumId: ID, userId: ID): [Album]
-    # Find an album with a certain albumId
-    album(albumId: ID!): Album
+    albums(id: ID, userId: ID): [Album]
+
+    # Find an album with a certain id
+    album(id: ID!): Album
   }
+
   type Album {
     # The Album's ID
-    albumId: ID
+    id: ID
+
     # The User ID that is associated this album
     userId: ID
+
     # The title of the album (Ex: "Nevermind")
     title: String
   }
@@ -26,11 +37,11 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    albums: async (rootObj, { albumId, userId }) => {
+    albums: async (rootObj, { id, userId }) => {
       const albums = await get('/albums');
 
-      if (albumId) {
-        return albums.filter(album => album.id === Number(albumId));
+      if (id) {
+        return albums.filter(album => album.id === Number(id));
       }
 
       if (userId) {
@@ -40,15 +51,7 @@ const resolvers = {
       return albums;
     },
 
-    album: async (rootObj, { albumId }) => {
-      const albums = await get('/albums');
-
-      return albums.find(album => album.id === Number(albumId));
-    }
-  },
-
-  Album: {
-    albumId: ({ id }) => id
+    album: async (rootObj, { id }) => await get(`/albums/${id}`)
   }
 };
 
